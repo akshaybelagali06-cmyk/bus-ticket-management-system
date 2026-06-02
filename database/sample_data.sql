cbd41538055812f7059c981f76260a3f9725c83b
Akshay <akshaybelagali06@gmail.com>
2026-06-02 20:34:35 +0530

    Added MySQL database files

---
+++ database/sample_data.sql
@@ -0,0 +1,8 @@
+USE bus_pass_management;
+
+INSERT INTO Student
+(name, department, year, phone)
+VALUES
+('Akshay', 'CSE', 3, '9876543210'),
+('Rahul', 'ECE', 4, '9988776655'),
+('Amogh', 'ISE', 2, '9123456780');
